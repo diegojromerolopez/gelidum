b59c525ed280d55643a9b497d60cf19592c1d25c
Diego J <diegojromerolopez@gmail.com>
2021-06-13 16:22:02 +0200

    Test time performance

--- tests/test_time_performance.py
+++ tests/test_time_performance.py
@@ -8,6 +8,34 @@ class TestTimePerformance(unittest.TestCase):
     def setUp(self) -> None:
         clear_frozen_classes()
 
+    def test_freeze_inplace_or_not(self):
+        class DummyAttrLevel3(object):
+            def __init__(self, attr: str):
+                self.attr = attr
+
+        class DummyAttrLevel2(object):
+            def __init__(self, attr: str):
+                self.attr = DummyAttrLevel3(attr)
+
+        class DummyAttrLevel1(object):
+            def __init__(self, attr: str):
+                self.attr = DummyAttrLevel2(attr)
+
+        class Dummy(object):
+            def __init__(self, attr: str):
+                self.attr = DummyAttrLevel1(attr)
+
+        dummy1 = Dummy(attr="1")
+        start_inplace = time.time()
+        freeze(dummy1, inplace=True)
+        spent_time_inplace = time.time() - start_inplace
+        dummy2 = Dummy(attr="1")
+        start_not_inplace = time.time()
+        freeze(dummy2, inplace=False)
+        spent_time_not_inplace = time.time() - start_not_inplace
+
+        self.assertLessEqual(spent_time_inplace, spent_time_not_inplace)
+
     def test_many_attr_class_freeze(self):
         class BigDummy(object):
             def __init__(self, attr: str):
