0d5350680f1a5f2464b2d2dc67aebe7131bb1c34
Diego J <diegojromerolopez@gmail.com>
2021-06-26 20:32:51 +0200

    frozenlist draft implementation

--- gelidum/collections/frozenlist.py
+++ gelidum/collections/frozenlist.py
@@ -1,30 +1,92 @@
-from typing import Any, Callable, Optional
-from gelidum import freeze
+from typing import Any, Callable, Optional, Sized, Union, Iterable, Reversible
+from gelidum import freeze, FrozenException
 from gelidum.frozen import FrozenBase
 
+__all__ = [
+    "frozenlist"
+]
 
-class ImmutableList(object):
-    def __init__(self, *args, freeze_func: Optional[Callable[[Any], FrozenBase]] = None):
-        if freeze_func is None:
-            def freeze_func(item: Any) -> FrozenBase:
-                return freeze(item, on_update="exception", on_freeze="copy")
-        self.__freeze = freeze_func
+FrozenList = Union[FrozenBase, Sized, Iterable, Reversible, "_List"]
 
-        if len(args) > 0:
-            self.__items = tuple(
-                self.__freeze(arg_i) for arg_i in args
-            )
-        else:
-            self.__items = tuple()
+def frozenlist(  # noqa
+        *args,
+        freeze_func: Optional[Callable[[Any], FrozenBase]] = None
+) -> FrozenList:
+    if freeze_func is None:
+        def freeze_func(item: Any) -> FrozenBase:
+            return freeze(item, on_update="exception", on_freeze="copy")
+    return freeze_func(_List(*args))
+
+
+class _List(object): # noqa
+    def __raise_immutable_exception(self):
+        raise FrozenException("'frozenlist' object is immutable")
+
+    def __init__(self, *args):
+        self.__items = tuple(args)
 
     def __getitem__(self, key) -> Any:
-        return self.__items[key]
+        try:
+            return self.__items[key]
+        except IndexError:
+            raise IndexError("frozenlist index out of range")
 
     def __len__(self) -> int:
         return len(self.__items)
 
-    def __add__(self, other) -> "ImmutableList":
-        frozen_other = self.__freeze(other)
-        return ImmutableList(
-            *(self.__items + (frozen_other,))
+    def __add__(self, other) -> FrozenList:
+        return frozenlist(
+            *(self.__items + (other,))
         )
+
+    def __contains__(self, item) -> bool:
+        return item in self.__items
+
+    def __reversed__(self) -> FrozenList:
+        return frozenlist(
+            *(reversed(self.__items))
+        )
+
+    def append(self, item) -> None:
+        self.__raise_immutable_exception()
+
+    def extend(self, iterable):
+        self.__raise_immutable_exception()
+
+    def insert(self, i, x):
+        self.__raise_immutable_exception()
+
+    def remove(self, x):
+        self.__raise_immutable_exception()
+
+    def pop(self, items):
+        self.__raise_immutable_exception()
+
+    def clear(self):
+        self.__raise_immutable_exception()
+
+    def index(self, x, start=None, end=None) -> int:
+        args = [x]
+        if start:
+            args.append(start)
+        if end:
+            args.append(end)
+        try:
+            return self.__items.index(*args)
+        except ValueError:
+            raise ValueError(f"{x} is not in frozenlist")
+
+    def count(self, x):
+        pass
+
+    def sort(self, *, key=None, reverse=False):
+        self.__raise_immutable_exception()
+
+    def reverse(self):
+        self.__raise_immutable_exception()
+
+    def copy(self) -> "_List":
+        """
+        frozenlist objects will only be
+        """
+        return self
--- tests/collection_tests/test_frozenlist.py
+++ tests/collection_tests/test_frozenlist.py
@@ -1,9 +1,80 @@
 import unittest
+from typing import Any
 
+from gelidum import FrozenException
 from gelidum.collections.frozenlist import frozenlist
+from gelidum.frozen import FrozenBase
 
 
 class TestFrozenlist(unittest.TestCase):  # noqa
+    def test_empty_construction(self):
+        frozen_list = frozenlist()
+
+        self.assertEqual(0, len(frozen_list))
+
+    def test_construction(self):
+        class Dummy:
+            def __init__(self, value: Any):
+                self.value = value
+
+        frozen_list = frozenlist(1, "2", Dummy(3))
+
+        self.assertTrue(isinstance(frozen_list, FrozenBase))
+        self.assertEqual(3, len(frozen_list))
+        self.assertEqual(1, frozen_list[0])
+        self.assertEqual("2", frozen_list[1])
+        self.assertTrue(isinstance(frozen_list[2], FrozenBase))
+        self.assertEqual(3, frozen_list[2].value)
+
+    def test_len(self):
+        frozen_list = frozenlist(1, 2, 3)
+
+        self.assertEqual(3, len(frozen_list))
+
+    def test_add(self):
+        frozen_list = frozenlist(1, 2, 3)
+        frozen_list2 = frozen_list + 2
+
+        self.assertEqual(3, len(frozen_list))
+        self.assertEqual(1, frozen_list[0])
+        self.assertEqual(2, frozen_list[1])
+        self.assertEqual(3, frozen_list[2])
+        self.assertEqual(4, len(frozen_list2))
+        self.assertEqual(1, frozen_list2[0])
+        self.assertEqual(2, frozen_list2[1])
+        self.assertEqual(3, frozen_list2[2])
+        self.assertEqual(2, frozen_list2[3])
+
+    def test_contains(self):
+        frozen_list = frozenlist(1, 2, 3)
+
+        self.assertTrue(1 in frozen_list)
+        self.assertFalse(9 in frozen_list)
+
+    def test_reversed(self):
+        class Dummy:
+            def __init__(self, value: Any):
+                self.value = value
+
+        frozen_list = frozenlist(1, 2, 3, Dummy(9))
+        reversed_frozen_list = reversed(frozen_list)
+
+        self.assertNotEqual(id(frozen_list), id(reversed_frozen_list))
+        self.assertEqual(4, len(frozen_list))
+        self.assertEqual(1, frozen_list[0])
+        self.assertEqual(2, frozen_list[1])
+        self.assertEqual(3, frozen_list[2])
+        self.assertEqual(3, frozen_list[2])
+        self.assertTrue(isinstance(frozen_list, FrozenBase))
+        self.assertEqual(9, frozen_list[3].value)
+
+        self.assertEqual(4, len(reversed_frozen_list))
+        self.assertTrue(isinstance(reversed_frozen_list, FrozenBase))
+        self.assertEqual(9, reversed_frozen_list[0].value)
+        self.assertEqual(3, reversed_frozen_list[1])
+        self.assertEqual(2, reversed_frozen_list[2])
+        self.assertEqual(1, reversed_frozen_list[3])
+
     def test_getitem(self):
         frozen_list = frozenlist(1, 2, 3)
 
@@ -13,8 +84,95 @@ class TestFrozenlist(unittest.TestCase):  # noqa
         self.assertEqual(1, frozen_list[0])
         self.assertEqual(2, frozen_list[1])
         self.assertEqual(3, frozen_list[2])
-        self.assertEqual("frozenlist index out of range", str(context.exception))
+        self.assertEqual("frozenlist index out of range",
+                         str(context.exception))
 
-    def test_tree(self):
-        cons(cons(1, 2), cons(3, 4))
+    def test_setitem_exception(self):
+        frozen_list = frozenlist(1, 2, 3)
+
+        with self.assertRaises(FrozenException) as context:
+            frozen_list[0] = 11
+
+        self.assertEqual("Can't set key '0' on immutable instance",
+                         str(context.exception))
+
+    def test_append_exception(self):
+        frozen_list = frozenlist(1, 2, 3)
+
+        with self.assertRaises(FrozenException) as context:
+            frozen_list.append(4)
+
+        self.assertEqual("'frozenlist' object is immutable",
+                         str(context.exception))
+
+    def test_extend_exception(self):
+        frozen_list = frozenlist(1, 2, 3)
+
+        with self.assertRaises(FrozenException) as context:
+            frozen_list.extend([4])
+
+        self.assertEqual("'frozenlist' object is immutable",
+                         str(context.exception))
+
+    def test_insert_exception(self):
+        frozen_list = frozenlist(1, 2, 3)
+
+        with self.assertRaises(FrozenException) as context:
+            frozen_list.insert(i=4, x=2)
+
+        self.assertEqual("'frozenlist' object is immutable",
+                         str(context.exception))
+
+    def test_remove_exception(self):
+        frozen_list = frozenlist(1, 2, 3)
+
+        with self.assertRaises(FrozenException) as context:
+            frozen_list.remove(4)
+
+        self.assertEqual("'frozenlist' object is immutable",
+                         str(context.exception))
+
+    def test_pop_exception(self):
+        frozen_list = frozenlist(1, 2, 3)
+
+        with self.assertRaises(FrozenException) as context:
+            frozen_list.pop(items=4)
+
+        self.assertEqual("'frozenlist' object is immutable",
+                         str(context.exception))
+
+    def test_clear_exception(self):
+        frozen_list = frozenlist(1, 2, 3)
+
+        with self.assertRaises(FrozenException) as context:
+            frozen_list.clear()
+
+        self.assertEqual("'frozenlist' object is immutable",
+                         str(context.exception))
+
+    def test_index_ok(self):
+        frozen_list = frozenlist(1, 2, 3)
+
+        index = frozen_list.index(2)
+
+        self.assertEqual(1, index)
+
+    def test_sort_exception(self):
+        frozen_list = frozenlist(1, 2, 3)
+
+        with self.assertRaises(FrozenException) as context:
+            frozen_list.sort()
+
+        self.assertEqual("'frozenlist' object is immutable",
+                         str(context.exception))
+
+    def test_reverse_exception(self):
+        frozen_list = frozenlist(1, 2, 3)
+
+        with self.assertRaises(FrozenException) as context:
+            frozen_list.reverse()
+
+        self.assertEqual("'frozenlist' object is immutable",
+                         str(context.exception))
+
 
