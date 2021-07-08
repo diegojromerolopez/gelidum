d7d718872f9d8b91790a63a200e3edb374dc1f29
Diego J <diegojromerolopez@gmail.com>
2021-07-08 17:49:06 +0200

    keys() and values() methods tests for frozendict

--- tests/collection_tests/test_frozendict.py
+++ tests/collection_tests/test_frozendict.py
@@ -1,7 +1,8 @@
 import unittest
-from typing import Any, Iterator
+from collections import ValuesView, KeysView
+from typing import Any
 
-from gelidum import FrozenException, freeze
+from gelidum import FrozenException
 from gelidum.collections.frozendict import frozendict
 from gelidum.frozen import FrozenBase
 
@@ -65,3 +66,44 @@ class TestFrozendict(unittest.TestCase):  # noqa
             "'frozendict' object is immutable",
             str(context_del.exception)
         )
+
+    def test_keys(self):
+        class Dummy:
+            def __init__(self, value: Any):
+                self.value = value
+
+        dummy = Dummy(3)
+        frozen_dict = frozendict({"one": 1, "two": "2", "three": dummy})
+        frozen_dict_keys = frozen_dict.keys()
+        frozen_dict_keys_iter = iter(frozen_dict_keys)
+        frozen_dict_keys_list = list(frozen_dict_keys)
+
+        self.assertTrue(isinstance(frozen_dict_keys, KeysView))
+        self.assertEqual("one", next(frozen_dict_keys_iter))
+        self.assertEqual("two", next(frozen_dict_keys_iter))
+        self.assertEqual("three", next(frozen_dict_keys_iter))
+        self.assertEqual(3, len(frozen_dict_keys_list))
+        self.assertTrue("one" in frozen_dict_keys_list)
+        self.assertTrue("two" in frozen_dict_keys_list)
+        self.assertTrue("three" in frozen_dict_keys_list)
+
+    def test_values(self):
+        class Dummy:
+            def __init__(self, value: Any):
+                self.value = value
+
+        dummy = Dummy(3)
+        frozen_dict = frozendict({"one": 1, "two": "2", "three": dummy})
+        frozen_dict_values = frozen_dict.values()
+        frozen_dict_values_iter = iter(frozen_dict.values())
+        frozen_dict_values_list = list(frozen_dict_values)
+
+        self.assertTrue(isinstance(frozen_dict_values, ValuesView))
+        self.assertEqual(1, next(frozen_dict_values_iter))
+        self.assertEqual("2", next(frozen_dict_values_iter))
+        self.assertEqual(frozen_dict["three"], next(frozen_dict_values_iter))
+        self.assertEqual(3, len(frozen_dict_values_list))
+        self.assertTrue(1 in frozen_dict_values_list)
+        self.assertTrue("2" in frozen_dict_values_list)
+        self.assertFalse(dummy in frozen_dict_values_list)
+        self.assertTrue(frozen_dict["three"] in frozen_dict_values_list)
