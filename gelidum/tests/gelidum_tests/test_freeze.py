15ea99a656cf84d7a77798ed4dfe6d6ce845b752
Diego J <diegojromerolopez@gmail.com>
2022-12-29 23:34:55 +0100

    New test: freezing dataclass objects

--- gelidum/tests/gelidum_tests/test_freeze.py
+++ gelidum/tests/gelidum_tests/test_freeze.py
@@ -136,6 +136,56 @@ class TestFreeze(unittest.TestCase):
         self.assertEqual(id(dummy), id(frozen_dummy_inplace))
         self.assertNotEqual(id(dummy), id(frozen_dummy_on_freeze_copy))
 
+    def test_freeze_nested_dataclass(self):
+        from dataclasses import dataclass
+
+        @dataclass
+        class ItemType:
+            id: int
+            name: str
+
+        @dataclass
+        class Item:
+            id: int
+            name: str
+            order: int
+            type: ItemType
+
+        @dataclass
+        class Database:
+            items: List[Item]
+
+        item_type1 = ItemType(1, "item type 1")
+        item_type2 = ItemType(2, "item type 2")
+
+        dummy_database = Database(
+            items=[
+                Item(1, "item 1", 5, item_type1),
+                Item(2, "item 2", 1, item_type1),
+                Item(3, "item 3", 99, item_type2)
+            ]
+        )
+        frozen_dummy_on_freeze_copy = freeze(dummy_database, on_freeze="copy")
+        frozen_dummy_inplace = freeze(dummy_database, on_freeze="inplace")
+
+        with self.assertRaises(FrozenException) as context_on_freeze_copy_items_assignment:
+            frozen_dummy_on_freeze_copy.items = []
+
+        with self.assertRaises(FrozenException) as context_on_freeze_copy_item_assignment:
+            frozen_dummy_on_freeze_copy.items[0] = Item(9, "item 9", 99, item_type1)
+
+        with self.assertRaises(FrozenException) as context_inplace:
+            frozen_dummy_inplace.attr2 = "2"
+
+        self.assertEqual("Can't assign attribute 'items' on immutable instance",
+                         str(context_on_freeze_copy_items_assignment.exception))
+        self.assertEqual("'frozenlist' object is immutable",
+                         str(context_on_freeze_copy_item_assignment.exception))
+        self.assertEqual("Can't assign attribute 'attr2' on immutable instance",
+                         str(context_inplace.exception))
+        self.assertEqual(id(dummy_database), id(frozen_dummy_inplace))
+        self.assertNotEqual(id(dummy_database), id(frozen_dummy_on_freeze_copy))
+
     def test_freeze_objects_of_same_class(self):
         class Dummy(object):
             def __init__(self, value: int):
